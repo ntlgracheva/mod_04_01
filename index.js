09095fec10c10bf849dede1961a5cfc65671e000
Natalia Gracheva <ntlgracheva@gmail.com>
2023-04-13 20:29:02 +0200

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+let productName = "Ноутбук";
+let productAmount = 5;
+let productCategory = "Персональные компьютеры";
+let productPrice = 500;
+
+console.log('Наименование товара: ', productName);
+console.log('Общая сумма товара: ', productAmount * productPrice, "$");
