0ed2673d858d7eb0794ac9a025e4c82eccb8db64
Natalia Gracheva <ntlgracheva@gmail.com>
2023-04-16 18:54:52 +0200

    to const

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let productName = "Ноутбук";
-let productAmount = 5;
-let productCategory = "Персональные компьютеры";
-let productPrice = 500;
+const productName = "Ноутбук";
+const productAmount = 5;
+const productCategory = "Персональные компьютеры";
+const productPrice = 500;
 
 console.log('Наименование товара: ', productName);
 console.log('Общая сумма товара: ', productAmount * productPrice, "$");
